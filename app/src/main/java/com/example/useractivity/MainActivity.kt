53711da53e61512866f0683f76c1695f5b155563
2Bmodular <2bmod@bglab.local>
2025-10-21 22:24:12 -0300

    Função de color dos botões Happy Sunny and Infinity

--- app/src/main/java/com/example/useractivity/MainActivity.kt
+++ app/src/main/java/com/example/useractivity/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.useractivity
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import org.w3c.dom.Text
@@ -29,11 +31,37 @@ class MainActivity : AppCompatActivity() {
 
         var novafrase = findViewById<TextView>(R.id.text_frase)
         var Botaofrase = findViewById<Button>(R.id.Bottom_novaFrase)
+        var imagemAll = findViewById<ImageView>(R.id.imagem_infinity)
+        var imagemHappy = findViewById<ImageView>(R.id.imagem_Emotion)
+        var imagemSunny = findViewById<ImageView>(R.id.Imagem_sunny)
+
+        imagemAll.setOnClickListener {
+            imagemHappy.setColorFilter(ContextCompat.getColor(this, R.color.black))
+            imagemSunny.setColorFilter(ContextCompat.getColor(this, R.color.black))
+            var frase = fraseRepository.getfrase(0)
+            novafrase.text = frase
+        }
+
+        imagemHappy.setOnClickListener {
+            imagemAll.setColorFilter(ContextCompat.getColor(this, R.color.black))
+            imagemSunny.setColorFilter(ContextCompat.getColor(this, R.color.black))
+            var frase = fraseRepository.getfrase(1)
+            novafrase.text = frase
+        }
+
+        imagemSunny.setOnClickListener {
+            imagemAll.setColorFilter(ContextCompat.getColor(this, R.color.black))
+            imagemHappy.setColorFilter(ContextCompat.getColor(this, R.color.black))
+            var frase = fraseRepository.getfrase(2)
+            novafrase.text = frase
+        }
 
         Botaofrase.setOnClickListener {
             var frase = fraseRepository.getfrase(0)
             novafrase.text = frase
         }
+
+
 
 
     }
