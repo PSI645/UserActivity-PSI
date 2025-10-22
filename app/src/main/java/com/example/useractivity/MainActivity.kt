e0ab2dd75f1c5a2a52f4ae86818d425b77638654
2Bmodular <2bmod@bglab.local>
2025-10-21 22:11:52 -0300

    Criação de função frases bt

--- app/src/main/java/com/example/useractivity/MainActivity.kt
+++ app/src/main/java/com/example/useractivity/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.useractivity
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val fraseRepository = FraseRepository2()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -20,5 +25,16 @@ class MainActivity : AppCompatActivity() {
         var nome = findViewById<TextView>(R.id.text_name)
         var nomeRecuperacao = this.getSharedPreferences("motivação", MODE_PRIVATE).getString("nome"."Olá PSI")
         nome.text = nomeRecuperacao
+
+
+        var novafrase = findViewById<TextView>(R.id.text_frase)
+        var Botaofrase = findViewById<Button>(R.id.Bottom_novaFrase)
+
+        Botaofrase.setOnClickListener {
+            var frase = fraseRepository.getfrase(0)
+            novafrase.text = frase
+        }
+
+
     }
 }
